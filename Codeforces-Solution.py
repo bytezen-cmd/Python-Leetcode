69a0a88a4b1e2d3fe6bba6b60cb119182d97f4b0
bytezen-cmd <bytepulse2025@outlook.com>
2025-10-19 23:34:53 +0530

    Added New Methods

--- Codeforces-Solution.py
+++ Codeforces-Solution.py
@@ -2,23 +2,31 @@ class Solution:
 
     @staticmethod
     def is_lucky(s) -> bool:
-        lucky = [char for char in s]
-        lucky = list(set(lucky))
-        if lucky != [7,4] or lucky != [4,7] or lucky != [4] or lucky != [7]:
-            return False
+        for char in s:
+            if char != "4" and char != "7":
+                return False
         return True
 
     @staticmethod
     def nearly_lucky() -> None:
         line = input()
         count = 0
-        for i in range(len(line)):
-            if line[i] == "4" or  line[i] == "7":
+        for char in line:
+            if (char == "4") or (char == "7"):
                 count += 1
-        str_count = str(count)
-        if Solution.is_lucky(str_count):
-            print("NO")
-        else:
+        if Solution.is_lucky(str(count)):
             print("YES")
+        else:
+            print("NO")
 
-Solution.nearly_lucky()
+    @staticmethod
+    def translation():
+        line1 = input()
+        line2 = input()
+        if line1[::-1] == line2:
+            print("YES")
+        else:
+            print("NO")
+
+
+Solution.translation()
